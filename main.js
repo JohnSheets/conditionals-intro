0362ee3957f3d309ca9d7f392d44e642b9bf2199
John Sheets <johnsheets410@gmail.com>
2023-02-15 09:51:36 -0500

    conditionals-intro

---
+++ main.js
@@ -0,0 +1,30 @@
+if (1 + 1 > 5) {
+    console.log("Lies!")
+}
+if (4 * 5 <= 20) {
+   console.log("True fact!");
+}
+if (6 - 2 >= 0) {
+    console.log("Lies!");
+}
+
+const likesDogs = true;
+
+if (likesDogs === true) {
+    console.log("you're a Dog person!"); 
+} else if (likesDogs === false) {
+    console.log("You're a Cat person!");
+};
+
+const grade = 88
+if (grade >= 0 && grade <= 69) {
+    console.log('You got an F');
+} else if (grade >= 70 && grade <= 76) {
+console.log('You got a D');
+} else if (grade >= 77 && grade <= 84) {
+console.log('You got a C');
+} else if (grade >= 84 && grade <= 92) {
+console.log('you got a B');
+} else if (grade >= 93 && grade <= 100) {
+    console.log('you got an A');
+}
